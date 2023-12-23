c2899663510e2b1d790590e61cd5202fc7fa7ff9
SovaSniper <SovaSniper@outlook.com>
2023-12-23 16:35:06 +1100

    renames and contract updates

---
+++ chainlink/43113/GenericLargeResponse.sol
@@ -0,0 +1,88 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
+import "@chainlink/contracts/src/v0.8/shared/access/ConfirmedOwner.sol";
+
+/**
+ * Request testnet LINK and ETH here: https://faucets.chain.link/
+ * Find information on LINK Token Contracts and get the latest ETH and LINK faucets here: https://docs.chain.link/docs/link-token-contracts/
+ */
+
+/**
+ * THIS IS AN EXAMPLE CONTRACT THAT USES HARDCODED VALUES FOR CLARITY.
+ * THIS IS AN EXAMPLE CONTRACT THAT USES UN-AUDITED CODE.
+ * DO NOT USE THIS CODE IN PRODUCTION.
+ */
+
+contract GenericLargeResponse is ChainlinkClient, ConfirmedOwner {
+    using Chainlink for Chainlink.Request;
+
+    // variable bytes(arbitrary-length raw byte data) returned in a single oracle response
+    bytes public data;
+    string public image_url;
+
+    bytes32 private jobId;
+    uint256 private fee;
+
+    /**
+     * @notice Initialize the link token and target oracle
+     * @dev The oracle address must be an Operator contract for multiword response
+     *
+     *
+     * Fuji Testnet details:
+     * Link Token: 0x0b9d5D9136855f6FEc3c0993feE6E9CE8a297846
+     * Oracle: 0x022EEA14A6010167ca026B32576D6686dD7e85d2 (Chainlink DevRel)
+     * jobId: 7da2702f37fd48e5b1b9a5715e3509b6
+     *
+     */
+    constructor() ConfirmedOwner(msg.sender) {
+        setChainlinkToken(0x0b9d5D9136855f6FEc3c0993feE6E9CE8a297846);
+        setChainlinkOracle(0x022EEA14A6010167ca026B32576D6686dD7e85d2);
+        jobId = "7da2702f37fd48e5b1b9a5715e3509b6";
+        fee = (1 * LINK_DIVISIBILITY) / 10; // 0,1 * 10**18 (Varies by network and job)
+    }
+
+    /**
+     * @notice Request variable bytes from the oracle
+     */
+    function requestBytes() public {
+        Chainlink.Request memory req = buildChainlinkRequest(
+            jobId,
+            address(this),
+            this.fulfillBytes.selector
+        );
+        req.add(
+            "get",
+            "https://ipfs.io/ipfs/QmZgsvrA1o1C8BGCrx6mHTqR1Ui1XqbCrtbMVrRLHtuPVD?filename=big-api-response.json"
+        );
+        req.add("path", "image");
+        sendChainlinkRequest(req, fee);
+    }
+
+    event RequestFulfilled(bytes32 indexed requestId, bytes indexed data);
+
+    /**
+     * @notice Fulfillment function for variable bytes
+     * @dev This is called by the oracle. recordChainlinkFulfillment must be used.
+     */
+    function fulfillBytes(
+        bytes32 requestId,
+        bytes memory bytesData
+    ) public recordChainlinkFulfillment(requestId) {
+        emit RequestFulfilled(requestId, bytesData);
+        data = bytesData;
+        image_url = string(data);
+    }
+
+    /**
+     * Allow withdraw of Link tokens from the contract
+     */
+    function withdrawLink() public onlyOwner {
+        LinkTokenInterface link = LinkTokenInterface(chainlinkTokenAddress());
+        require(
+            link.transfer(msg.sender, link.balanceOf(address(this))),
+            "Unable to transfer"
+        );
+    }
+}
